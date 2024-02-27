2f4164202527d79eb18b4e8da3cb601e347fd10c
Shahir <34528340+cirem52@users.noreply.github.com>
2024-02-27 15:56:29 -0500

    Add files via upload

--- PE3_4.py
+++ PE3_4.py
@@ -1,9 +1,16 @@
-a = int(input("Enter an integer"))
-b = input("Enter another integer")
-c = input("Enter a third integer")
+a = input("Enter an integer ")
+b = input("Enter another integer ")
+c = input("Enter a third integer ")
+
+print("Before Sorting:" + a, b, c)
+
+a = int(a)
+b = int(b)
+c = int(c)
+
 
 
 highest_value = max(a,b,c)
 lowest_value = min(a,b,c)
-middle_value = (a + b + c) - (highest_value - lowest_value)
+middle_value = (a + b + c) - (highest_value + lowest_value)
 print(lowest_value, middle_value, highest_value)
