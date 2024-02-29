28eee46fd6116828e4a7ca3c45cd257794697cd7
Shahir <34528340+cirem52@users.noreply.github.com>
2024-02-29 15:22:54 -0500

    Delete PE3_3.py

--- PE3_3.py
+++
@@ -1,4 +0,0 @@
-height = float(input("Enter your height (In cm): "))
-weight = float(input("Enter your weight (In kg): "))
-bmi = round((weight / (height**2)) * 703,3)
-print("Your bmi is: " + str(bmi))
