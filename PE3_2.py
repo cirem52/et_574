3c7dda7829145d00fffb5007a6856676dea2ad44
Shahir <34528340+cirem52@users.noreply.github.com>
2024-02-29 15:22:47 -0500

    Delete PE3_2.py

--- PE3_2.py
+++
@@ -1,8 +0,0 @@
-bill = float(input("Input the bill amount: "))
-tip = int(input("Enter the tip amount (Exclude the percentage sign): "))
-
-tipAmount = tip / 100
-
-print("Your tip is " + str(round(bill * tipAmount, 2)))
-print("Total: " + str(round(bill + (bill * tipAmount), 2)))
-
